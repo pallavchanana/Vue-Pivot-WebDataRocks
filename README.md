17c5363c6bfbcc323ab021830e043a1d3024f40d
pallavchanana <pcpallav@gmail.com>
2020-05-30 11:01:38 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## How to install
+- Install Nodejs from [Nodejs Official Page](https://nodejs.org/en/)
+- Open your terminal
+
+### Using Git (recommended)
+
+1.  Clone the project from github. Change "myproject" to your project name.
+
+```bash
+git clone https://github.com/pallavchanana/vue-pivot-webdatarocks.git ./myproject
+```
+
+### Using manual download ZIP
+
+1.  Download repository
+2.  Uncompress to your desired directory
+
+### Install npm dependencies after installing (Git or manual download)
+
+```bash
+cd myproject
+npm install
+npm update
+```
+
+### Compiles and hot-reloads for development
+
+```bash
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```bash
+npm run build
---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <WebDataRocks report=report toolbar></WebDataRocks>
+  </div>
+</template>
+
+<script>
+import WebDataRocks from './components/WebDataRocks.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WebDataRocks
+  },
+    data() {
+        return {
+            report: null
+        }
+    },
+    mounted() {
+        this.report = {
+            
+           
+                dataSource: {
+            filename: 'https://cdn.webdatarocks.com/data/data.csv'
+        },
+                "slice": {
+        "rows": [
+            {
+                "uniqueName": "Country"
+            },
+            {
+                "uniqueName": "Category"
+            }
+        ],
+        "columns": [
+            {
+                "uniqueName": "Business Type"
+            },
+            {
+                "uniqueName": "Measures"
+            }
+        ],
+        "measures": [
+            {
+                "uniqueName": "Price",
+                "aggregation": "sum"
+            }
+        ]
+    },
+    "conditions": [
+        {
+            "formula": "#value > 2000000",
+            "measure": "Price",
+            "format": {
+                "backgroundColor": "#0598df",
+                "color": "#FFFFFF",
+                "fontFamily": "Arial",
+                "fontSize": "12px"
+            }
+        }
+    ]
+            }
+    }
+
+}
+</script>
+
+<style>
+</style>
+
+
